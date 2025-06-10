17f5e06fc4d3a00e797118735440a8f17e52eadd
c0a24224 <c0a24224a2@edu.teu.ac.jp>
2025-06-10 16:01:34 +0900

    練習5：右から左へ

--- pygame_image.py
+++ pygame_image.py
@@ -11,12 +11,12 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")   #背景画像のSurfac
     kk_img = pg.image.load("fig/3.png")
-    kk_img = pg.transform.flip(kk_img, True, True)
+    kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [-tmr, 0])
         screen.blit(kk_img,[300,200])
         pg.display.update()
         tmr += 1        
